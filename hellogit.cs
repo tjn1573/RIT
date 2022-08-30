f349d3c0806716e5cda5af0171d559dae6fa852e
tjn1573 <112191086+tjn1573@users.noreply.github.com>
2022-08-30 12:40:46 -0400

    Add files via upload

---
+++ hellogit.cs
@@ -0,0 +1,12 @@
+﻿namespace hello_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thomas Noschese!");
+            Console.WriteLine("IGM 105.09");
+            Console.WriteLine("Helllo Git program");
+        }
+    }
+}
